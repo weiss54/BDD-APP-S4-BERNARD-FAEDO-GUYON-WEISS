87bb4381d4b452d3108d352687fa0ead5ff5a92f
Leo BERNARD <leo.bernard1@etu.univ-lorraine.fr>
2022-03-03 11:25:28 +0100

    qrgr

--- requet3.sql
+++ requet3.sql
@@ -1,3 +1,3 @@
 -- lister les plateformes dont la base installée est >= 10 000 000 --
 
-select name from platform where install_base >= 10000000;
+select * from platform where install_base >= 10000000;
--- requet4.sql
+++ requet4.sql
@@ -1,1 +1,3 @@
 -- lister 442 jeux à partir du 21173ème --
+
+select * from game where id >= 21173 limit 442;
