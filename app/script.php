209093665810ca90932a521a5b4ca94563880ab2
TheoFaedo <73793294+TheoFaedo@users.noreply.github.com>
2022-03-03 11:42:46 +0100

    a

--- app/script.php
+++ app/script.php
@@ -1,4 +1,9 @@
 <?php
+
+
+
+use app\Requete;
+
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
@@ -11,4 +16,10 @@ $db->addConnection(parse_ini_file("db.config.ini"));
 $db->setAsGlobal();
 $db->bootEloquent(); //On lance Eloquent
 
+$rq = new Requete();
 
+$rq->requete1();
+$rq->requete2();
+$rq->requete3();
+$rq->requete4();
+$rq->requete5();
--- app/src/Requete.php
+++ app/src/Requete.php
@@ -1,16 +1,21 @@
 <?php
 
+namespace app;
+
 use app\models\Game;
+use app\models\Compagny;
 
 class Requete
 {
 
     public function requete1() {
-        return Game::select ('name')->where('name', 'like', '%Mario%');
+        return Game::select('name')->where('name', 'like', '%Mario%');
     }
 
     public function requete2(){
         return Compagny::where('name', 'like', '%Mario%');
     }
+
+    
 
 }
