8e4618e4107946b0be149984c240608a8b2bb79e
TheoFaedo <73793294+TheoFaedo@users.noreply.github.com>
2022-03-03 11:25:41 +0100

    ajout importations

--- app/script.php
+++ app/script.php
@@ -1,2 +1,12 @@
 <?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
+use Illuminate\Database\Capsule\Manager as DB;
+
+$db = new DB();
+
+$db->addConnection(IntegrateurBDD::$dbconfig);
+ 
+$db->setAsGlobal();
+$db->bootEloquent(); //On lance Eloquent
