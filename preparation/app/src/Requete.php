e98893333c32e5eff5156fe4dc9ff32d1e0abe3f
Leo BERNARD <leo.bernard1@etu.univ-lorraine.fr>
2022-03-08 16:51:20 +0100

    euh.... je suis pas convaincu

--- preparation/app/src/Requete.php
+++ preparation/app/src/Requete.php
@@ -2,14 +2,18 @@
 
 namespace app;
 
-use app\models\Annonce;
-use app\models\Photo;
+use td2prep\models\Annonce;
+use td2prep\models\Photo;
 
 
 class Requete
 {
 
     public function requete1() {
-        return ;
+        return Photo::join('Annonce')->select('id')->where('Annonce.id', '=', '22')->get();
+    }
+
+    public function requete2() {
+        return Photo::join('Annonce')->select('id')->where('Annonce.id', '=', '22')->where('Photo.taille_octet', '>=', '100000')->get();
     }
 }
