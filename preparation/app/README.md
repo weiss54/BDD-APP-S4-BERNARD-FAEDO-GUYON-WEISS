6502c9db6c2b6a49c2dd89570264bfbc481cc385
weiss54 <72012859+weiss54@users.noreply.github.com>
2022-03-09 13:59:11 +0100

    Finalisation td2 preparatoire

--- preparation/app/README.md
+++ preparation/app/README.md
@@ -1,1 +1,7 @@
 # BDD-APP-S4-BERNARD-FAEDO-GUYON-WEISS
+## 1) Schéma SQL -> Voir dans le dossier documents
+## 2) Voir les fichiers sql: Annonce et Photo
+## 3) Voir le fichier Requete.sql
+## 4) insert into Photo values (99, 'file99.png', to_date('09/03/22'), 22);
+## 5) insert into CategorieAnnonce values (42, 22);
+##    insert into CategorieAnnonce values (73, 22);
--- preparation/app/src/Requete.php
+++ preparation/app/src/Requete.php
@@ -13,7 +13,24 @@ class Requete
         return Photo::join('Annonce')->select('id')->where('Annonce.id', '=', '22')->get();
     }
 
+    public function requete1v2() {
+        return Annonce->hasMany()->where('id', '22')->get();
+    }
+
     public function requete2() {
         return Photo::join('Annonce')->select('id')->where('Annonce.id', '=', '22', 'and','Photo.taille_octet', '>=', '100000')->get();
     }
+
+    public function requete2v2() {
+        return Annonce->hasMany()->where('id', '22')->where('taille_octet', '>', '100000')->get();
+    }
+
+    public function requete3() {
+        return Photo->annonce()->groupeBy('count')->having('count', '>', '3')->get();
+    }
+
+    public function requete4() {
+        return Photo->annonce()->where('taille_octet', '>', '100000')->get();
+    }
+
 }
