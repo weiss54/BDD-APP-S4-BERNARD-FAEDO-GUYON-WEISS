bec7e274bf4172b7dceac2c13d4c6a5f890e5751
TheoFaedo <73793294+TheoFaedo@users.noreply.github.com>
2022-03-09 14:32:58 +0100

    Update Requete.php

--- preparation/app/src/Requete.php
+++ preparation/app/src/Requete.php
@@ -9,6 +9,7 @@ use td2prep\models\Photo;
 class Requete
 {
 
+<<<<<<< Updated upstream
     public function requete1()
     {
         return Photo::join('Annonce')->select('id')->where('Annonce.id', '=', '22')->get();
@@ -38,6 +39,25 @@ class Requete
     public function requete4()
     {
         return Photo::annonce()->where('taille_octet', '>', '100000')->get();
+=======
+    public function requete1() {
+        return Annonce::find(22)->photos()->get();
+    }
+
+    public function requete2() {
+        return Annonce::find(22)->photos()->where('taille_octet', '>', '100000')->get();
+    }
+
+    public function requete3() {
+        $annonces = Annonce::has('photos', '>', '3')->get();
+        return $annonces;
+    }
+
+    public function requete4() {
+        return Annonce::whereAs('photos', function($q){
+            $q->where('taille_octet', '>', 100000);
+        })->get();
+>>>>>>> Stashed changes
     }
 
 }
