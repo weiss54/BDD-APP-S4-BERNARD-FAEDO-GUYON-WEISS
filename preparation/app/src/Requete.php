da61f9c7f20ba02178ffd75687628e082442fc95
weiss54 <72012859+weiss54@users.noreply.github.com>
2022-03-09 14:02:04 +0100

    Rectification erreurs

--- preparation/app/src/Requete.php
+++ preparation/app/src/Requete.php
@@ -14,7 +14,7 @@ class Requete
     }
 
     public function requete1v2() {
-        return Annonce->hasMany()->where('id', '22')->get();
+        return Annonce::photos()->where('id', '22')->get();
     }
 
     public function requete2() {
@@ -22,15 +22,15 @@ class Requete
     }
 
     public function requete2v2() {
-        return Annonce->hasMany()->where('id', '22')->where('taille_octet', '>', '100000')->get();
+        return Annonce::photos()->where('id', '22')->where('taille_octet', '>', '100000')->get();
     }
 
     public function requete3() {
-        return Photo->annonce()->groupeBy('count')->having('count', '>', '3')->get();
+        return Photo::annonce()->groupeBy('count')->having('count', '>', '3')->get();
     }
 
     public function requete4() {
-        return Photo->annonce()->where('taille_octet', '>', '100000')->get();
+        return Photo::annonce()->where('taille_octet', '>', '100000')->get();
     }
 
 }
