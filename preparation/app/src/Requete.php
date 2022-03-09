11f464889fef434d2cd4fd273894496f16674e71
weiss54 <72012859+weiss54@users.noreply.github.com>
2022-03-09 14:07:41 +0100

    Rectification erreurs

--- preparation/app/src/Requete.php
+++ preparation/app/src/Requete.php
@@ -9,27 +9,34 @@ use td2prep\models\Photo;
 class Requete
 {
 
-    public function requete1() {
+    public function requete1()
+    {
         return Photo::join('Annonce')->select('id')->where('Annonce.id', '=', '22')->get();
     }
 
-    public function requete1v2() {
-        return Annonce::photos()->where('id', '22')->get();
+    public function requete1v2()
+    {
+        $p = Annonce::find(22);
+        return $p->photos()->get();
     }
 
-    public function requete2() {
-        return Photo::join('Annonce')->select('id')->where('Annonce.id', '=', '22', 'and','Photo.taille_octet', '>=', '100000')->get();
+    public function requete2()
+    {
+        return Photo::join('Annonce')->select('id')->where('Annonce.id', '=', '22', 'and', 'Photo.taille_octet', '>=', '100000')->get();
     }
 
-    public function requete2v2() {
-        return Annonce::photos()->where('id', '22')->where('taille_octet', '>', '100000')->get();
+    public function requete2v2()
+    {
+        $p = Annonce::find(22);
+        return $p->photos()->where('taille_octet', '>', '100000')->get();
     }
 
     public function requete3() {
         return Photo::annonce()->groupeBy('count')->having('count', '>', '3')->get();
     }
 
-    public function requete4() {
+    public function requete4()
+    {
         return Photo::annonce()->where('taille_octet', '>', '100000')->get();
     }
 
