301f536d92a092cef8d68942eafe5a84e9fcdc8c
TheoFaedo <73793294+TheoFaedo@users.noreply.github.com>
2022-03-24 00:41:52 +0100

    Update Script.php

--- app/preparation/Script.php
+++ app/preparation/Script.php
@@ -7,7 +7,7 @@ echo "### QUESTION 2\n";
 
 echo $faker->randomNumber(3, false) . " " . "Avenue " . $faker->name() . "\n" . $faker->name() . " " . $faker->randomNumber(5, false);
 
-echo "\n\n### QUESTION 3\n";
+echo "\n\n### QUESTION 3\n"; 
 
 $dateTime = DateTime::createFromFormat('Y/m/d (H:i)', "2017/02/16 (16:15)"); //Date écrite vers DateTime
 
