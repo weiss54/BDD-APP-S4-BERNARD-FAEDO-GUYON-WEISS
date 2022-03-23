90a68b2ff85778675dadd8a101c0ff332093a233
TheoFaedo <73793294+TheoFaedo@users.noreply.github.com>
2022-03-24 00:40:11 +0100

    a

---
+++ app/preparation/Script.php
@@ -0,0 +1,14 @@
+<?php
+require_once '../vendor/autoload.php';
+
+$faker = Faker\Factory::create();
+
+echo "### QUESTION 2\n";
+
+echo $faker->randomNumber(3, false) . " " . "Avenue " . $faker->name() . "\n" . $faker->name() . " " . $faker->randomNumber(5, false);
+
+echo "\n\n### QUESTION 3\n";
+
+$dateTime = DateTime::createFromFormat('Y/m/d (H:i)', "2017/02/16 (16:15)"); //Date écrite vers DateTime
+
+echo "\ndate enonce : " . $dateTime->format('Y/m/d(H:i)');
--- app/vendor/composer/autoload_psr4.php
+++ app/vendor/composer/autoload_psr4.php
@@ -7,7 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'voku\\' => array($vendorDir . '/voku/portable-ascii/src/voku'),
-    'teezer\\' => array($baseDir . '/src'),
+    'app\\' => array($baseDir . '/src'),
     'Symfony\\Polyfill\\Php80\\' => array($vendorDir . '/symfony/polyfill-php80'),
     'Symfony\\Polyfill\\Php73\\' => array($vendorDir . '/symfony/polyfill-php73'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
@@ -23,11 +23,12 @@ return array(
     'Psr\\SimpleCache\\' => array($vendorDir . '/psr/simple-cache/src'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
     'Psr\\Container\\' => array($vendorDir . '/psr/container/src'),
-    'Illuminate\\Support\\' => array($vendorDir . '/illuminate/macroable', $vendorDir . '/illuminate/collections', $vendorDir . '/illuminate/support'),
+    'Illuminate\\Support\\' => array($vendorDir . '/illuminate/collections', $vendorDir . '/illuminate/macroable', $vendorDir . '/illuminate/support'),
     'Illuminate\\Database\\' => array($vendorDir . '/illuminate/database'),
     'Illuminate\\Contracts\\' => array($vendorDir . '/illuminate/contracts'),
     'Illuminate\\Container\\' => array($vendorDir . '/illuminate/container'),
     'FastRoute\\' => array($vendorDir . '/nikic/fast-route/src'),
+    'Faker\\' => array($vendorDir . '/fakerphp/faker/src/Faker'),
     'Doctrine\\Inflector\\' => array($vendorDir . '/doctrine/inflector/lib/Doctrine/Inflector'),
     'Carbon\\' => array($vendorDir . '/nesbot/carbon/src/Carbon'),
 );
