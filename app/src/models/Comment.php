25cee7da276ea0fd692bf671b087fcd95d592586
TheoFaedo <73793294+TheoFaedo@users.noreply.github.com>
2022-03-24 08:41:18 +0100

    timestamp

--- app/src/models/Comment.php
+++ app/src/models/Comment.php
@@ -5,7 +5,7 @@ namespace app\models;
 class Comment extends \Illuminate\Database\Eloquent\Model
 {
 
-    public $timestamps = false;
+    public $timestamps = true;
     protected $table = 'comment';
     protected $primaryKey = 'id';
 
